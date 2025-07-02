4510b3de837406505ed19590417e64bce094ac7c
Gwangmin Choi <crazy4u5@naver.com>
2025-07-03 00:34:41 +0900

    chore: yarn dlx @yarnpkg/sdks의 sdk 폴더, .vscode 폴더 추가 (근거: https://yarnpkg.com/getting-started/qa#which-files-should-be-gitignored), .vscode는 일단 ignore되게 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { fileURLToPath } from 'node:url';
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url)); 
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
@@ -9,11 +13,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
+    ignorePatterns: [
+      'node_modules/',
+      '.yarn/',
+      '.vscode',
+      'dist/',
+      'build/',
+      '.next/',
+      'public/',
+    ],
     extends: ['next/core-web-vitals', 'next/typescript'],
   }),
 ]
 
 export default defineConfig([
+  includeIgnoreFile(gitignorePath),
   eslintConfig,
   eslintConfigPrettier,
 ]);
