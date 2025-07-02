26f04de6a4ba2c48809cacf6840695161eec8a55
Ori <sdgwsld@gmail.com>
2025-07-03 02:40:19 +0900

    chore/style: prettier 정상적으로 실행되는 것 확인

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
 import { FlatCompat } from '@eslint/eslintrc';
-import { defineConfig } from "eslint/config";
-import { includeIgnoreFile } from "@eslint/compat";
+import { defineConfig } from 'eslint/config';
+import { includeIgnoreFile } from '@eslint/compat';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import { fileURLToPath } from 'node:url';
 
-const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url)); 
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
@@ -24,7 +24,7 @@ const eslintConfig = [
     ],
     extends: ['next/core-web-vitals', 'next/typescript'],
   }),
-]
+];
 
 export default defineConfig([
   includeIgnoreFile(gitignorePath),
