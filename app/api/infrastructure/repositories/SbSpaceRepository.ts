fecbaf1e153276ac058d44190fa373f707c651b3
zu0p <zu0p@kakao.com>
2025-07-04 17:03:12 +0900

    feat: repository 구현체 공통 supabase 변수, 생성자 설정

--- app/api/infrastructure/repositories/SbSpaceRepository.ts
+++ app/api/infrastructure/repositories/SbSpaceRepository.ts
@@ -1,17 +1,23 @@
-import { SpaceRoomView } from "../../domain/entities/SpaceRoomView";
-import { SaveRequest, UpdateRequest } from "../../domain/repository/spaceRequest";
-import { SpaceRepository } from "../../domain/repository/SpaceRepository";
+import { SpaceRoomView } from '../../domain/entities/SpaceRoomView';
+import {
+  SaveRequest,
+  UpdateRequest,
+} from '../../domain/repository/spaceRequest';
+import { SpaceRepository } from '../../domain/repository/SpaceRepository';
+import { SupabaseClient } from '@supabase/supabase-js';
 
-export class SbSpaceRepository implements SpaceRepository{
+export class SbSpaceRepository implements SpaceRepository {
+  private supabase;
+
+  constructor(supabase: SupabaseClient) {
+    this.supabase = supabase;
+  }
+
   async findById(id: number): Promise<SpaceRoomView[]> {
     return [];
   }
 
-  async save(space: SaveRequest): Promise<void> {
-    
-  }
+  async save(space: SaveRequest): Promise<void> {}
 
-  async update(space: UpdateRequest): Promise<void> {
-    
-  }
+  async update(space: UpdateRequest): Promise<void> {}
 }
