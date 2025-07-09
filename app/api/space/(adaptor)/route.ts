f5ac60f6bd2ae5721fea338c560304b15cfd9f22
kanghyun <dev.kanghyun@gmail.com>
2025-07-09 23:01:07 +0900

    fix: conflict 수정

--- app/api/space/(adaptor)/route.ts
+++ app/api/space/(adaptor)/route.ts
@@ -6,25 +6,17 @@ import { SbRoomRepository } from '../../infrastructure/repositories/SbRoomReposi
 
 export async function GET(request: NextRequest) {
   try {
-<<<<<<< Updated upstream
-    // Supabase 클라이언트 생성
-    const supabase = await createClient();
-    const spaceRepository = new SbSpaceRepository(supabase);
-    const getSpaceUsecase = new GetSpaceUsecase(spaceRepository);
-    const spaces = await getSpaceUsecase.execute(1);
-=======
     const { searchParams } = new URL(request.url);
     const spaceId = Number(searchParams.get('spaceId'));
 
     const supabase = await createClient();
     const spaceRepository = new SbSpaceRepository(supabase);
-    const roomRepository = new SbRoomRepository(supabase); // Assuming you have a RoomRepository similar to SpaceRepository
+    const roomRepository = new SbRoomRepository(supabase);
     const getSpaceUsecase = new GetSpaceUsecase(
       spaceRepository,
       roomRepository
     );
     const spaces = await getSpaceUsecase.execute(spaceId);
->>>>>>> Stashed changes
     return NextResponse.json(spaces);
   } catch (error) {
     console.error('Error fetching spaces:', error);
