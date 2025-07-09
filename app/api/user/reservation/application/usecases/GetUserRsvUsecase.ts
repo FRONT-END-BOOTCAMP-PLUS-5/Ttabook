1232f806403441a90f89fac3a4c55ae29f484f32
Ori <sdgwsld@gmail.com>
2025-07-09 22:00:40 +0900

    feat: GetUserRsvUsecase에 예약 현황 조회 기능 추가

--- app/api/user/reservation/application/usecases/GetUserRsvUsecase.ts
+++ app/api/user/reservation/application/usecases/GetUserRsvUsecase.ts
@@ -1,24 +1,37 @@
-import { Rsv } from "@/app/api/domain/entities/Rsv";
-import { RsvRepository } from "@/app/api/domain/repository/RsvRepository";
-import { GetUserRsvDto } from "../dto/GetUserRsvDto";
+import { Rsv } from '@/app/api/domain/entities/Rsv';
+import { RsvRepository } from '@/app/api/domain/repository/RsvRepository';
 
 export class GetUserRsvUsecase {
-    private repository: RsvRepository;
-    constructor(repository: RsvRepository) {
-        this.repository = repository;
+  private repository: RsvRepository;
+  constructor(repository: RsvRepository) {
+    this.repository = repository;
+  }
+
+  async execute(userId: string) {
+    const reservations: Rsv[] | null =
+      await this.repository.findByUserId(userId);
+
+    // HACK: 나중에 공간 시간 관리 할 수 있는 기능 만들면 반드시 수정할 것
+    const RESERVATION_START_TIME = 9;
+    const RESERVATION_END_TIME = 18;
+
+    const reservationStatus: number[] = new Array(
+      RESERVATION_END_TIME - RESERVATION_START_TIME
+    ).fill(0);
+
+    if (!reservations) {
+      return reservationStatus;
     }
-    async execute(userId: string) {
-        const reservations : Rsv[] | null = await this.repository.findByUserId(userId);
-        
-        return reservations?.map((rsv: Rsv) => {
-            return new GetUserRsvDto(
-                rsv.spaceId,
-                rsv.space ? rsv.space.name : '',
-                rsv.roomId,
-                rsv.room ? rsv.room.name : '',
-                rsv.startTime,
-                rsv.endTime
-            )
-        }) || [];
-    }
+
+    reservations.forEach((e) => {
+      for (
+        let i = e.startTime.getHours() - RESERVATION_START_TIME;
+        i <= e.endTime.getHours() - RESERVATION_START_TIME;
+        i++
+      )
+        reservationStatus[i] = 1;
+    });
+
+    return reservationStatus;
+  }
 }
