a8181ab7001e4d5dd3d34046e7940122ccdbce24
zu0p <zu0p@kakao.com>
2025-07-11 12:19:09 +0900

    feat: input 컴포넌트생성

---
+++ ds/components/atoms/input/Input.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { InputProps } from './Input.types';
+import '../../../styles/globals.css';
+import styles from './Input.module.css';
+import { CaptionText } from '../text/textWrapper';
+
+const Input: React.FC<InputProps> = ({
+  _size = 'md',
+  isFullWidth = false,
+  variant = 'primary',
+  error,
+  required,
+  ...props
+}) => {
+  const className = [
+    styles['ttabook-input'],
+    styles[`ttabook-input--${error ? 'danger' : variant}`],
+    styles[`ttabook-input--${_size}`],
+    isFullWidth ? styles[`ttabook-input--fullWidth`] : '',
+  ];
+
+  return (
+    <div className={styles['ttabook-input-wrapper']}>
+      {required && (
+        <CaptionText variant="danger" size="md" style={{ lineHeight: '3px' }}>
+          *
+        </CaptionText>
+      )}
+      <input className={className.filter(Boolean).join(' ')} {...props} />
+      {error && <CaptionText variant="danger">{error}</CaptionText>}
+    </div>
+  );
+};
+
+export default Input;
---
+++ ds/components/atoms/input/Input.types.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import { InputSize } from "@/ds/styles/tokens/input/size";
+import { InputVariant } from "@/ds/styles/tokens/input/variant";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>{
+  _size?: InputSize;
+  isFullWidth?:boolean;
+  variant?:InputVariant;
+  error?:string;
+  required?:boolean
+}
