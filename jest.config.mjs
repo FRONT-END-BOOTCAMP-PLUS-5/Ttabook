5130ed8e84d0865e616eb413e069bae6dd89b2a1
Ori <sdgwsld@gmail.com>
2025-07-13 15:44:54 +0900

    chore: Test coverage 기준 80%로 조정

--- jest.config.mjs
+++ jest.config.mjs
@@ -20,10 +20,10 @@ const config = {
   ],
   coverageThreshold: {
     './lib/**/*.{js,ts}': {
-      branches: 85,
-      functions: 85,
-      lines: 85,
-      statements: 85,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
