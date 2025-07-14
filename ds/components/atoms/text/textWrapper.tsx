593fd3f3651d5ffe4100eeea47c886f04fc8df47
zu0p <zu0p@kakao.com>
2025-07-14 16:55:45 +0900

    fix: time tile에서 text를 사용하려다가 className은 이미 지정된 것이라 넣어줘야하는 부분이 생겨서 Text수정함

--- ds/components/atoms/text/textWrapper.tsx
+++ ds/components/atoms/text/textWrapper.tsx
@@ -1,72 +1,60 @@
-import Text from "./Text"
-import { TextProps } from "./Text.types"
+import Text from './Text';
+import { TextProps } from './Text.types';
 
-const TitleText:React.FC<TextProps> = ({
+const TitleText: React.FC<TextProps> = ({
   children,
   size = 'xl',
   variant = 'primary',
+  className,
   ...props
 }) => {
-  return(
-    <Text
-      size={size}
-      variant={variant}
-      {...props}
-    >
+  return (
+    <Text size={size} variant={variant} className={className} {...props}>
       {children}
     </Text>
-  )
-}
+  );
+};
 
-const SubTitleText:React.FC<TextProps> = ({
+const SubTitleText: React.FC<TextProps> = ({
   children,
   size = 'lg',
   variant = 'secondary',
+  className,
   ...props
 }) => {
   return (
-    <Text
-      size={size}
-      variant={variant}
-      {...props}
-    >
+    <Text size={size} variant={variant} className={className} {...props}>
       {children}
     </Text>
-  )
-}
+  );
+};
 
-const DescText:React.FC<TextProps> = ({
+const DescText: React.FC<TextProps> = ({
   children,
   size = 'md',
   variant = 'primary',
+  className,
   ...props
 }) => {
   return (
-    <Text
-      size={size}
-      variant={variant}
-      {...props}
-    >
+    <Text size={size} variant={variant} className={className} {...props}>
       {children}
     </Text>
-  )
-}
+  );
+};
 
-const CaptionText:React.FC<TextProps> = ({
+const CaptionText: React.FC<TextProps> = ({
   children,
   size = 'sm',
   variant = 'primary',
+  className,
   ...props
 }) => {
   return (
-    <Text
-      size={size}
-      variant={variant}
-      {...props}
-    >
+    <Text size={size} variant={variant} className={className} {...props}>
       {children}
     </Text>
-  )
-}
+  );
+};
 
-export { TitleText, SubTitleText, DescText, CaptionText }
+export { TitleText, SubTitleText, DescText, CaptionText };
