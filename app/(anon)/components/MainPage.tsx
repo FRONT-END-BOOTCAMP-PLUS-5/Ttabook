c038d7d43cd2f6728540265975a3425e602ba0c1
zu0p <zu0p@kakao.com>
2025-07-16 10:58:23 +0900

    style: main page style 추가

--- app/(anon)/components/MainPage.tsx
+++ app/(anon)/components/MainPage.tsx
@@ -3,16 +3,19 @@
 import React from 'react';
 import Canvas from './Canvas';
 import styles from './MainPage.module.css';
+import { TitleText } from '@/ds/components/atoms/text/textWrapper';
 
 const MainPage: React.FC = () => {
   const spaceName = '멋쟁이 사자';
 
   return (
     <div className={styles.mainPageContainer}>
-        <h1 className={styles.title}>{spaceName}의 공간</h1>
-        <Canvas />
+      <TitleText variant="primary" style={{ fontSize: 32, fontWeight: 'bold' }}>
+        {spaceName}의 공간
+      </TitleText>
+      <Canvas />
     </div>
-);
+  );
 };
 
 export default MainPage;
