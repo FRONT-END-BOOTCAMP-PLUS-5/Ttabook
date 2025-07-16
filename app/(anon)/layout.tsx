8337c4971118cddfa1ea376faa5e50f9ba6fe3e1
zu0p <zu0p@kakao.com>
2025-07-16 10:59:38 +0900

    feat: (anon)/ layout 추가 - 로그인여부에 따른 header추가

---
+++ app/(anon)/layout.tsx
@@ -0,0 +1,34 @@
+'use client';
+import Footer from '@/ds/components/molecules/footer/Footer';
+import { useSession } from '../providers/SessionProvider';
+import LoggedInHeader from '../components/LoggedInHeader';
+import LoggedOutHeader from '../components/LoggedOutHeader';
+import { useState } from 'react';
+import SigninModal from './components/modal/signin/SigninModal';
+import SignupModal from './components/modal/signup/SignupModal';
+
+export default function AnonLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { user, logout } = useSession();
+  const [signinOpen, setSigninOpen] = useState<boolean>(false);
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      {signinOpen && <SigninModal onClose={setSigninOpen} />}
+      {signupOpen && <SignupModal onClose={setSignupOpen} />}
+      {user ? (
+        <LoggedInHeader onLogout={logout} />
+      ) : (
+        <LoggedOutHeader onSignin={setSigninOpen} onSignup={setSignupOpen} />
+      )}
+      {children}
+      <Footer>
+        <></>
+      </Footer>
+    </div>
+  );
+}
