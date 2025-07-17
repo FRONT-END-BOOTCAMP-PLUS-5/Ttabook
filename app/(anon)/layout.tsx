65905bd8afb03b3e8c9ddf0bbfcd16f788beda3f
zu0p <zu0p@kakao.com>
2025-07-17 11:18:07 +0900

    fix: modal open에 대한 처리 통합

--- app/(anon)/layout.tsx
+++ app/(anon)/layout.tsx
@@ -3,27 +3,34 @@ import Footer from '@/ds/components/molecules/footer/Footer';
 import { useSession } from '../providers/SessionProvider';
 import LoggedInHeader from '../components/LoggedInHeader';
 import LoggedOutHeader from '../components/LoggedOutHeader';
-import { useState } from 'react';
 import SigninModal from './components/modals/signin/SigninModal';
 import SignupModal from './components/modals/signup/SignupModal';
+import { useModalStore } from '@/hooks/useModal';
 
 export default function AnonLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { user, logout } = useSession();
-  const [signinOpen, setSigninOpen] = useState<boolean>(false);
-  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const { isAuthenticated, logout } = useSession();
+  const { isModalOpen, openModal, closeModal } = useModalStore();
 
+  console.log(isAuthenticated);
   return (
     <div style={{ position: 'relative' }}>
-      {signinOpen && <SigninModal onClose={setSigninOpen} />}
-      {signupOpen && <SignupModal onClose={setSignupOpen} />}
-      {user ? (
+      {isModalOpen('signin') && (
+        <SigninModal onClose={() => closeModal('signin')} />
+      )}
+      {isModalOpen('signup') && (
+        <SignupModal onClose={() => closeModal('signup')} />
+      )}
+      {isAuthenticated ? (
         <LoggedInHeader onLogout={logout} />
       ) : (
-        <LoggedOutHeader onSignin={setSigninOpen} onSignup={setSignupOpen} />
+        <LoggedOutHeader
+          onSignin={() => openModal('signin')}
+          onSignup={() => openModal('signup')}
+        />
       )}
       {children}
       <Footer />
